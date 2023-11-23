cd34cebad85b0abee7279006818a248cbfc0eb6e
kohii <kohii.tokyo@gmail.com>
2023-11-24 07:45:14 +0900

    add title on drawer

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -1,37 +1,30 @@
 import { on } from "events";
+import { IconButton } from "./icon-button";
+import { CloseIcon } from "./icons/close-icon";
 
 type DrawerProps = {
 	children: React.ReactNode;
 	onClose: () => void;
+	title: string;
 };
 
-export function Drawer({ children, onClose }: DrawerProps) {
+export function Drawer({
+	children,
+	onClose,
+	title
+}: DrawerProps) {
 	return (
 		<div
 			className=" grid fixed overflow-hidden z-50 right-0 top-0 bg-white h-screen max-w-full w-[1000px] shadow-2xl"
 			style={{
-				gridTemplateRows: "56px minmax(0, 1fr)",
+				gridTemplateRows: "64px minmax(0, 1fr)",
 			}}
 		>
 			<div style={{ gridRow: 1 }} className="flex items-center pl-2">
-				<button
-					className="top-0 left-0 p-2"
-					onClick={onClose}
-				>
-					<svg
-						className="w-6 h-6 text-gray-600"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth={2}
-							d="M6 18L18 6M6 6l12 12"
-						/>
-					</svg>
-				</button>
+				<IconButton onClick={onClose} icon={<CloseIcon className="h-6 w-6" />} label="Close" />
+				<h3 className="text-l text-gray-900 ml-2">
+					{title}
+				</h3>
 			</div>
 			<div style={{ gridRow: 2 }} className="overflow-x-auto">
 				{children}
