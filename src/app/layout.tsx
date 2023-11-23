c50ab27f12fbcf077fca7aa194692cae5d70512f
kohii <kohii.tokyo@gmail.com>
2023-11-24 07:44:10 +0900

    tweak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-full font-sans">
+    <html lang="en" className="h-full">
       <body className={inter.className + " h-full bg-white leading-tight"}>
         <Providers>
           {children}
