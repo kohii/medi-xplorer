81648a647954016f5769b9fefac4c31ae4df5d91
kohii <kohii.tokyo@gmail.com>
2023-11-28 22:41:27 +0900

    tweak

--- src/features/shinryoukoui-basic-tab/section-heading.tsx
+++ src/features/shinryoukoui-basic-tab/section-heading.tsx
@@ -8,7 +8,7 @@ type SectionHeadingProps = {
 
 export function SectionHeading({ children, className }: SectionHeadingProps) {
 	return (
-		<h4 className={twMerge("text-lg font-medium text-slate-600 tracking-wider mt-5 mb-2", className)}>
+		<h4 className={twMerge("text-lg font-medium text-slate-600 tracking-wider mt-5 mb-1.5", className)}>
 			{children}
 		</h4>
 	);
