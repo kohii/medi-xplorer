5fa22a0e6ffb4c4ee14f65de64daf8425ff4c35a
kohii <kohii.tokyo@gmail.com>
2023-11-28 20:32:08 +0900

    tweak

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -21,7 +21,7 @@ export function Button({
 			data-modal-hide="default-modal"
 			className={
 				twMerge(
-					"text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800",
+					"text-white bg-blue-700 hover:bg-blue-800 focus:ring-[1px] focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800",
 					disabled && "opacity-50 cursor-not-allowed",
 					className,
 				)}
