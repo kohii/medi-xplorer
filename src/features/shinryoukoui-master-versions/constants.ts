3e69673d614f5faf43947342ec361851bc60faa2
kohii <kohii.tokyo@gmail.com>
2023-12-08 21:01:25 +0900

    refactor

--- src/features/shinryoukoui-master-versions/constants.ts
+++ src/features/shinryoukoui-master-versions/constants.ts
@@ -1,14 +1,17 @@
 import versions from "./shinryoukoui-master-versions.json" assert { type: "json" };
 
-export const SHINRYOUKOUI_MASTER_VERSION_LIST: {
-	key: string;
-	label: string;
-}[] = versions;
+// sort in descending order
+versions.sort((a, b) => b.localeCompare(a));
 
-export const SHINRYOUKOUI_MASTER_VERSION_KEYS = SHINRYOUKOUI_MASTER_VERSION_LIST.map(item => item.key);
+export const SHINRYOUKOUI_MASTER_VERSION_KEYS: string[] = versions;
 
-export const LATEST_SHINRYOUKOUI_MASTER_VERSION = SHINRYOUKOUI_MASTER_VERSION_LIST[0].key;
+export const LATEST_SHINRYOUKOUI_MASTER_VERSION = SHINRYOUKOUI_MASTER_VERSION_KEYS[0];
 
-export type ShinryoukouiMasterVersion = typeof SHINRYOUKOUI_MASTER_VERSION_LIST[number]["key"];
+export const SHINRYOUKOUI_MASTER_VERSION_LIST = versions.map(v => ({
+	key: v,
+	label: `${+v.slice(0, 4)}年${+v.slice(4, 6)}月${+v.slice(6, 8)}日`
+}));
+
+export type ShinryoukouiMasterVersion = string;
 
 export const MASTER_VERSION_SEARCH_PARAM_NAME = "v";
