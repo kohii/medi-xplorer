b78aac80671905eaef19f82108ba7fadc686689f
kohii <kohii.tokyo@gmail.com>
2023-12-09 14:46:35 +0900

    add bundle-analyzer & make project es module

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withBundleAnalyzer =
+  process.env.ANALYZE === "true"
+    ? (await import("@next/bundle-analyzer")).default({ enabled: true })
+    : (config) => config;
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = withBundleAnalyzer({
+  reactStrictMode: true,
+  swcMinify: true,
+});
+
+export default nextConfig;
