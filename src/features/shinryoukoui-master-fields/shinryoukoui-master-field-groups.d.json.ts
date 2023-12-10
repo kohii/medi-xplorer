a2cbe5c62176522943871691cceb2e79e7422bc6
kohii <kohii.tokyo@gmail.com>
2023-12-10 11:23:32 +0900

    extract field definitions to separated json files

---
+++ src/features/shinryoukoui-master-fields/shinryoukoui-master-field-groups.d.json.ts
@@ -0,0 +1,4 @@
+import { FieldGroup } from "@/features/fields/types";
+declare type _FieldGroupName = "診療行為省略名称" | "データ規格名" | "新又は現点数" | "きざみ値" | "上限回数" | "注加算" | "上下限年齢" | "基準適合識別" | "旧点数" | "施設基準①～⑩" | "コード表用番号（アルファベット部を除く）" | "告示・通知関連番号（アルファベット部を除く）" | "年齢加算①" | "年齢加算②" | "年齢加算③" | "年齢加算④";
+declare const data: (FieldGroup & { name: _FieldGroupName })[];
+export default data;
