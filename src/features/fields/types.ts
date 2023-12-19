9c9d7870d310ce0827de87e40b6cbdedc2494512
kohii <kohii.tokyo@gmail.com>
2023-12-19 22:09:45 +0900

    rafactor: make field definitions typescript const
    remove field group definition (not used)

--- src/features/fields/types.ts
+++ src/features/fields/types.ts
@@ -1,33 +1,33 @@
 export type Field = Readonly<{
-	/** 項番 */
-	seq: number;
-	/** 項目名 */
-	name: string;
-	/** 内容 */
-	description: string;
-	/** モード */
-	mode: "numeric" | "text" | "date" | "alphanumeric";
-	/** コード定義 */
-	codes?: readonly {
-		/** コード */
-		code: string;
-		/** 名称 */
-		name: string;
-		/** このコード値が有効な条件 */
-		condition?: Readonly<{
-			/** 項番 */
-			seq: number;
-			/** 値 */
-			value: readonly string[];
-		}>;
-		/** 簡潔さ重視の独自に定義した名称 */
-		alias?: string;
-	}[];
-	/** {@link codes}が指定されていた場合に、そのコード以外の値が入っているかどうか */
-	allowFreeValue?: boolean;
+  /** 項番 */
+  seq: number;
+  /** 項目名 */
+  name: string;
+  /** 内容 */
+  description: string;
+  /** モード */
+  mode: "numeric" | "text" | "date" | "alphanumeric";
+  /** コード定義 */
+  codes?: readonly {
+    /** コード */
+    code: string;
+    /** 名称 */
+    name: string;
+    /** このコード値が有効な条件 */
+    condition?: Readonly<{
+      /** 項番 */
+      seq: number;
+      /** 値 */
+      value: readonly string[];
+    }>;
+    /** 簡潔さ重視の独自に定義した名称 */
+    alias?: string;
+  }[];
+  /** {@link codes}が指定されていた場合に、そのコード以外の値が入っているかどうか */
+  allowFreeValue?: boolean;
 }>;
 
 export type FieldGroup = Readonly<{
-	name: string; // グループの項目名
-	description?: string; // 内容
+  name: string; // グループの項目名
+  description?: string; // 内容
 }>;
