24dc762510ba619be69b0c7f89fe1657a8682eee
kohii <kohii.tokyo@gmail.com>
2023-12-20 16:24:04 +0900

    refactor: use url instead of useState for selected tab (#25)

--- src/app/s/detail.tsx
+++ src/app/s/detail.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 import { Tabs } from "@/components/tabs";
 import { VSplit, VSplitItem } from "@/components/v-split";
+import { useUpdateSearchParams } from "@/hooks/use-update-search-params";
 
 import { ShinryoukouiBasicTab } from "../../features/shinryoukoui-basic-tab/shinryoukoui-basic-tab";
 import { ShinryoukouiRawTab } from "../../features/shinryoukoui-raw-tab/shinryoukoui-raw-tab";
@@ -9,11 +10,32 @@ import { ShinryoukouiRawTab } from "../../features/shinryoukoui-raw-tab/shinryou
 
 const tabs = [{
   label: "詳細",
-  value: "detail",
+  value: "basic",
 }, {
   label: "Raw",
   value: "raw",
-}];
+}] as const;
+const tabValues = tabs.map(t => t.value);
+type Tab = typeof tabValues[number];
+
+
+function useSelectedTab() {
+  const searchParams = useSearchParams();
+  const selectedTabInSearchParam = searchParams.get("tab");
+  const selectedTab: Tab = selectedTabInSearchParam && tabValues.includes(selectedTabInSearchParam as Tab) ?
+    selectedTabInSearchParam as Tab :
+    tabValues[0];
+
+  const updateSearchParams = useUpdateSearchParams();
+  const setSelectedTab = (tab: Tab) => {
+    updateSearchParams({
+      tab: tab === tabs[0]["value"] ? undefined : tab,
+    }, {
+      mode: "replace",
+    });
+  };
+  return [selectedTab, setSelectedTab] as const;
+}
 
 type DetailProps = {
   row: string[];
@@ -21,7 +43,8 @@ type DetailProps = {
 }
 
 export function Detail({ row, rows }: DetailProps) {
-  const [selectedTab, setSelectedTab] = useState("detail");
+  const [selectedTab, setSelectedTab] = useSelectedTab();
+
   return (
     <div className="h-full">
       <VSplit gridTemplateRows="min-content minmax(0, 1fr)">
@@ -30,8 +53,8 @@ export function Detail({ row, rows }: DetailProps) {
         </VSplitItem>
         <VSplitItem pos={2} overflow="auto">
           <div className="p-4">
+            {selectedTab === "basic" && <ShinryoukouiBasicTab row={row} rows={rows} />}
             {selectedTab === "raw" && <ShinryoukouiRawTab row={row} />}
-            {selectedTab === "detail" && <ShinryoukouiBasicTab row={row} rows={rows} />}
           </div>
         </VSplitItem>
       </VSplit>
