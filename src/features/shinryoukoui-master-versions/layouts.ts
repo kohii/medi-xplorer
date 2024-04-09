441f3e281e8ba6f188d7cc2df89b1614b69d6691
kohii <kohii.tokyo@gmail.com>
2024-04-09 22:57:55 +0900

    update shisetsukijun for R6

--- src/features/shinryoukoui-master-versions/layouts.ts
+++ src/features/shinryoukoui-master-versions/layouts.ts
@@ -3,16 +3,19 @@ export type ShinryoukouiMasterLayoutVersion = "2022" | "2024";
 export type ShinryoukouiMasterLayout = {
   version: ShinryoukouiMasterLayoutVersion;
   from: string;
+  shisetsuKijunFileName: string;
 };
 
 export const SHINRYOUKOUI_MASTER_LAYOUTS: ShinryoukouiMasterLayout[] = [
   {
     version: "2022",
     from: "20220401",
+    shisetsuKijunFileName: "shisetsukijun_20231020.tsv",
   },
   {
     version: "2024",
     from: "20240329",
+    shisetsuKijunFileName: "shisetsukijun_20240329.tsv",
   },
 ];
 
@@ -26,4 +29,13 @@ export function getLayoutVersion(date: string): ShinryoukouiMasterLayoutVersion 
     }
   }
   return SHINRYOUKOUI_MASTER_LAYOUTS[SHINRYOUKOUI_MASTER_LAYOUTS.length - 1].version;
+}
+
+export function getShinryoukouiLayoutFor(
+  version: ShinryoukouiMasterLayoutVersion,
+): ShinryoukouiMasterLayout {
+  return (
+    SHINRYOUKOUI_MASTER_LAYOUTS.find((layout) => layout.version === version) ??
+    SHINRYOUKOUI_MASTER_LAYOUTS[SHINRYOUKOUI_MASTER_LAYOUTS.length - 1]
+  );
 }
