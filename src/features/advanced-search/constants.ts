a0b7b94ccd1c83525f94449846721df57d187b87
kohii <kohii.tokyo@gmail.com>
2025-04-28 16:41:47 +0900

    tweak: dedup in and not-in values

--- src/features/advanced-search/constants.ts
+++ src/features/advanced-search/constants.ts
@@ -2,7 +2,6 @@ import type { FieldFilterItem } from "@/features/search/types";
 import type { ShinryoukouiMasterFieldName } from "@/features/shinryoukoui-master-fields/shinryoukoui-master-fields";
 import { splitByWhitespace } from "@/utils/text";
 
-
 export type AdvancedSearchItem = {
   field: ShinryoukouiMasterFieldName;
   operatorKind: AdvancedSearchOperatorKind;
@@ -18,118 +17,137 @@ type Option = {
   toFieldFilterItem(item: AdvancedSearchItem): FieldFilterItem;
 };
 
-export const advancedSearchOperatorOptions = [{
-  kind: "eq",
-  label: "が次と一致する",
-  operator: ":",
-  negative: false,
-  toFieldFilterItem(item: AdvancedSearchItem) {
-    return {
-      fieldKey: item.field,
-      operator: ":",
-      value: item.value.trim(),
-      negative: false,
-    };
-  },
-}, {
-  kind: "not-eq",
-  label: "が次と一致しない",
-  operator: ":",
-  negative: true,
-  toFieldFilterItem(item: AdvancedSearchItem) {
-    return {
-      fieldKey: item.field,
-      operator: ":",
-      value: item.value.trim(),
-      negative: true,
-    };
-  },
-}, {
-  kind: "in",
-  label: "が次のいずれかに一致する",
-  operator: ":",
-  negative: false,
-  toFieldFilterItem(item: AdvancedSearchItem) {
-    let values = (item.restValues ? [item.value, ...item.restValues] : [item.value]);
-    // migrate spaces to commas
-    values = values.flatMap(value => splitByWhitespace(value)).map(value => value.trim()).filter(Boolean);
-    return {
-      fieldKey: item.field,
-      operator: ":",
-      value: values.join(","),
-      negative: false,
-    };
-  },
-}, {
-  kind: "not-in",
-  label: "が次のいずれでもない",
-  operator: ":",
-  negative: true,
-  toFieldFilterItem(item: AdvancedSearchItem) {
-    let values = (item.restValues ? [item.value, ...item.restValues] : [item.value]);
-    // migrate spaces to commas
-    values = values.flatMap(value => splitByWhitespace(value)).map(value => value.trim()).filter(Boolean);
-    return {
-      fieldKey: item.field,
-      operator: ":",
-      value: values.join(","),
-      negative: true,
-    };
-  }
-}, {
-  kind: "gt",
-  label: "が次より大きい",
-  operator: ">",
-  negative: false,
-  toFieldFilterItem(item: AdvancedSearchItem) {
-    return {
-      fieldKey: item.field,
-      operator: ":>",
-      value: item.value.trim(),
-      negative: false,
-    };
-  },
-}, {
-  kind: "gte",
-  label: "が次以上",
-  operator: ">=",
-  negative: false,
-  toFieldFilterItem(item: AdvancedSearchItem) {
-    return {
-      fieldKey: item.field,
-      operator: ":>=",
-      value: item.value.trim(),
-      negative: false,
-    };
-  },
-}, {
-  kind: "lt",
-  label: "が次より小さい",
-  operator: "<",
-  negative: false,
-  toFieldFilterItem(item: AdvancedSearchItem) {
-    return {
-      fieldKey: item.field,
-      operator: ":<",
-      value: item.value,
-      negative: false,
-    };
-  },
-}, {
-  kind: "lte",
-  label: "が次以下",
-  operator: "<=",
-  negative: false,
-  toFieldFilterItem(item: AdvancedSearchItem) {
-    return {
-      fieldKey: item.field,
-      operator: ":<=",
-      value: item.value,
-      negative: false,
-    };
-  },
-}] satisfies Option[];
+function normalizeMultiValues(values: string[]) {
+  const migratedValues = values
+    .flatMap((value) => splitByWhitespace(value))
+    .map((value) => value.trim())
+    .filter(Boolean);
+  return [...new Set(migratedValues)];
+}
 
-export const advancedSearchItemOperatorKinds = advancedSearchOperatorOptions.map((option) => option.kind);
+export const advancedSearchOperatorOptions = [
+  {
+    kind: "eq",
+    label: "が次と一致する",
+    operator: ":",
+    negative: false,
+    toFieldFilterItem(item: AdvancedSearchItem) {
+      return {
+        fieldKey: item.field,
+        operator: ":",
+        value: item.value.trim(),
+        negative: false,
+      };
+    },
+  },
+  {
+    kind: "not-eq",
+    label: "が次と一致しない",
+    operator: ":",
+    negative: true,
+    toFieldFilterItem(item: AdvancedSearchItem) {
+      return {
+        fieldKey: item.field,
+        operator: ":",
+        value: item.value.trim(),
+        negative: true,
+      };
+    },
+  },
+  {
+    kind: "in",
+    label: "が次のいずれかに一致する",
+    operator: ":",
+    negative: false,
+    toFieldFilterItem(item: AdvancedSearchItem) {
+      const values = item.restValues ? [item.value, ...item.restValues] : [item.value];
+      // migrate spaces to commas
+      const value = normalizeMultiValues(values).join(",");
+      return {
+        fieldKey: item.field,
+        operator: ":",
+        value,
+        negative: false,
+      };
+    },
+  },
+  {
+    kind: "not-in",
+    label: "が次のいずれでもない",
+    operator: ":",
+    negative: true,
+    toFieldFilterItem(item: AdvancedSearchItem) {
+      const values = item.restValues ? [item.value, ...item.restValues] : [item.value];
+      // migrate spaces to commas
+      const value = normalizeMultiValues(values).join(",");
+      return {
+        fieldKey: item.field,
+        operator: ":",
+        value,
+        negative: true,
+      };
+    },
+  },
+  {
+    kind: "gt",
+    label: "が次より大きい",
+    operator: ">",
+    negative: false,
+    toFieldFilterItem(item: AdvancedSearchItem) {
+      return {
+        fieldKey: item.field,
+        operator: ":>",
+        value: item.value.trim(),
+        negative: false,
+      };
+    },
+  },
+  {
+    kind: "gte",
+    label: "が次以上",
+    operator: ">=",
+    negative: false,
+    toFieldFilterItem(item: AdvancedSearchItem) {
+      return {
+        fieldKey: item.field,
+        operator: ":>=",
+        value: item.value.trim(),
+        negative: false,
+      };
+    },
+  },
+  {
+    kind: "lt",
+    label: "が次より小さい",
+    operator: "<",
+    negative: false,
+    toFieldFilterItem(item: AdvancedSearchItem) {
+      return {
+        fieldKey: item.field,
+        operator: ":<",
+        value: item.value,
+        negative: false,
+      };
+    },
+  },
+  {
+    kind: "lte",
+    label: "が次以下",
+    operator: "<=",
+    negative: false,
+    toFieldFilterItem(item: AdvancedSearchItem) {
+      return {
+        fieldKey: item.field,
+        operator: ":<=",
+        value: item.value,
+        negative: false,
+      };
+    },
+  },
+] satisfies Option[];
 
-export type AdvancedSearchOperatorKind = typeof advancedSearchOperatorOptions[number]["kind"];
+export const advancedSearchItemOperatorKinds = advancedSearchOperatorOptions.map(
+  (option) => option.kind,
+);
+
+export type AdvancedSearchOperatorKind = (typeof advancedSearchOperatorOptions)[number]["kind"];
